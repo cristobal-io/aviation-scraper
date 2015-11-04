3afd2e2601c18c0893df994f2802a2aaa8561722
cristobal-io <me@cristobal.io>
2015-11-05 00:01:31 +0100

    retrieving airports by destination

--- airline_destinations.js
+++ airline_destinations.js
@@ -5,7 +5,7 @@ var scraperjs = require('scraperjs');
 scraperjs.StaticScraper.create('https://en.wikipedia.org/wiki/Category:Lists_of_airline_destinations')
   .scrape(function ($) {
     return $(".mw-category li a").map(function () {
-       return {
+      return {
         name: $(this).text().replace(/ destinations$/, ""),
         destinationsLink: $(this).attr("href")
       };
@@ -16,7 +16,9 @@ scraperjs.StaticScraper.create('https://en.wikipedia.org/wiki/Category:Lists_of_
     fs.writeFile(filename,
       JSON.stringify(destinationPages, null, 2),
       function (err) {
-        if (err) { throw err; }
+        if (err) {
+          throw err;
+        }
         console.log("Saved %s", filename);
       }
     );
@@ -25,64 +27,66 @@ scraperjs.StaticScraper.create('https://en.wikipedia.org/wiki/Category:Lists_of_
 // this is intended for the destinations page
 // Only has the code to be run on the browser directly
 
-  $("#mw-content-text").map(function() {
-    return {
-      origin: $(".mw-headline").map(function() {
-        return {
-          origin: $(this).text().replace(/Scheduled destinations from /, ""),
-          destinations: $("table tbody td").text()
-        }
-      }).get()
-      
-    };
-  })
+$("#mw-content-text").map(function () {
+  return {
+    origin: $(".mw-headline").map(function () {
+      return {
+        origin: $(this).text().replace(/Scheduled destinations from /, ""),
+        destinations: $("table tbody td").text()
+      }
+    }).get()
 
-  $("h2").next(".wikitable")
+  };
+})
 
-    $("#mw-content-text").map(function() {
-    return {
-      origin: $(".mw-headline").map(function() {
-        return {
-          origin: $(this).text().replace(/Scheduled destinations from /, ""),
-          destinations: $("h2").next(".wikitable").innerText
-        }
-      }).get()
-      
-    };
-  })
+$("h2").next(".wikitable")
+
+$("#mw-content-text").map(function () {
+  return {
+    origin: $(".mw-headline").map(function () {
+      return {
+        origin: $(this).text().replace(/Scheduled destinations from /, ""),
+        destinations: $("h2").next(".wikitable").innerText
+      }
+    }).get()
+
+  };
+})
 
 
 //  This returns 
 
-      $("#mw-content-text").map(function() {
-    return {
-      origin: $("h2").map(function() {
-        return {
-          origin: $(this).text().replace(/Scheduled destinations from /, ""),
-          destinations: $(this).next(".wikitable").map(function(index, elem) {
-            var destinations = [];
-            var tableContent = $(this).find("tr");
-            for (var i = 0; i < tableContent.length; i++) {
-              var destination = {};
-              for (var i = 0; i < Things.length; i++) {
-                Things[i]
-              };
-              tableContent[i]
+$("#mw-content-text").map(function () {
+  return {
+    origin: $("h2").map(function () {
+      return {
+        origin: $(this).text().replace(/Scheduled destinations from /, ""),
+        destinations: $(this).next(".wikitable").map(function (index, elem) {
+          var destinations = [];
+          var $originFrom = $(".mw-headline");
+          var $headers = $(this).find("th");
+          var $tableContent = $(this).find("tr td");
+          for (var k = 0; k < $originFrom.length; k++) {
+            
+            console.log("from : " + $($originFrom[k]).text());
+            for (var i = 0, j = 0; i < $tableContent.length; i++, j++) {
+              console.log("headers in loop " + j);
+              var textHeaders = $($headers[j]).text();
+              console.log(textHeaders);
+              console.log("table content in loop");
+              var textTableContent = $($tableContent[i]).text()
+              console.log(textTableContent);
+
+              if (j > $headers.length -2) {j = -1} ;
             };
-            console.log(JSON.stringify(tableContent));
-            return {
-              valueOfThis: $(this).find("td")
-              // City: $(this).
-              // Country:
-              // IATA:
-              // ICAO:
-              // Airport;
-            };
-          })
-        }
-      }).get()
-    };
-  })
+
+          };
+          return destinations;
+        })
+      }
+    }).get()
+  };
+})
 
 
 // This returns the first city of the table, with a for loop we can iterate over all 
@@ -97,16 +101,16 @@ var headersText = [];
 var $headers = $("th");
 
 // Loop through grabbing everything
-var $rows = $(".wikitable tr").each(function(index) {
+var $rows = $(".wikitable tr").each(function (index) {
   $cells = $(this).find("td");
   myRows[index] = {};
 
-  $cells.each(function(cellIndex) {
+  $cells.each(function (cellIndex) {
     // Set the header text
-    if(headersText[cellIndex] === undefined) {
+    if (headersText[cellIndex] === undefined) {
       headersText[cellIndex] = $($headers[cellIndex]).text();
     }
     // Update the row object with the header/cell combo
     myRows[index][headersText[cellIndex]] = $(this).text();
-  });    
+  });
 });
