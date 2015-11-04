7f6092258a8c34a4963333e28c08cf9ab69bac06
cristobal-io <me@cristobal.io>
2015-11-04 16:53:53 +0100

    table convert to object

--- airline_destinations.js
+++ airline_destinations.js
@@ -28,9 +28,85 @@ scraperjs.StaticScraper.create('https://en.wikipedia.org/wiki/Category:Lists_of_
   $("#mw-content-text").map(function() {
     return {
       origin: $(".mw-headline").map(function() {
-        return $(this).text().replace(/Scheduled destinations from /, "")
-      }).get(),
-      destinations: $("table tbody td").text()
+        return {
+          origin: $(this).text().replace(/Scheduled destinations from /, ""),
+          destinations: $("table tbody td").text()
+        }
+      }).get()
       
     };
   })
+
+  $("h2").next(".wikitable")
+
+    $("#mw-content-text").map(function() {
+    return {
+      origin: $(".mw-headline").map(function() {
+        return {
+          origin: $(this).text().replace(/Scheduled destinations from /, ""),
+          destinations: $("h2").next(".wikitable").innerText
+        }
+      }).get()
+      
+    };
+  })
+
+
+//  This returns 
+
+      $("#mw-content-text").map(function() {
+    return {
+      origin: $("h2").map(function() {
+        return {
+          origin: $(this).text().replace(/Scheduled destinations from /, ""),
+          destinations: $(this).next(".wikitable").map(function(index, elem) {
+            var destinations = [];
+            var tableContent = $(this).find("tr");
+            for (var i = 0; i < tableContent.length; i++) {
+              var destination = {};
+              for (var i = 0; i < Things.length; i++) {
+                Things[i]
+              };
+              tableContent[i]
+            };
+            console.log(JSON.stringify(tableContent));
+            return {
+              valueOfThis: $(this).find("td")
+              // City: $(this).
+              // Country:
+              // IATA:
+              // ICAO:
+              // Airport;
+            };
+          })
+        }
+      }).get()
+    };
+  })
+
+
+// This returns the first city of the table, with a for loop we can iterate over all 
+// 
+$(".wikitable").find("td")[0]
+
+
+// this returns objects of each Airport
+// 
+var myRows = [];
+var headersText = [];
+var $headers = $("th");
+
+// Loop through grabbing everything
+var $rows = $(".wikitable tr").each(function(index) {
+  $cells = $(this).find("td");
+  myRows[index] = {};
+
+  $cells.each(function(cellIndex) {
+    // Set the header text
+    if(headersText[cellIndex] === undefined) {
+      headersText[cellIndex] = $($headers[cellIndex]).text();
+    }
+    // Update the row object with the header/cell combo
+    myRows[index][headersText[cellIndex]] = $(this).text();
+  });    
+});
