b19e52b3bff27f162e1f186ef66ade422a53d1b5
cristobal-io <me@cristobal.io>
2015-11-14 12:00:07 +0100

    Class where we set a proper callback workflow.

--- airline_scraper.js
+++ airline_scraper.js
@@ -5,51 +5,58 @@
   var async = require("async");
   var scrapers = require("./scrapers/");
   var _ = require("lodash");
-
+  var destinationsFile = "./data/destination_pages.json";
   var BASE_URL = "https://en.wikipedia.org";
 
-  var airlines = require("./data/destination_pages.json");
+  var airlines = require(destinationsFile);
 
-  function getRoutes(options, key, callback) {
+  function getScraperType(options, callback) {
     var url = BASE_URL + options.destinationsLink;
 
     console.log("Getting scraper for %s from %s", options.name, url);
     sjs.StaticScraper.create(url)
       .scrape(scrapers["type_of_scrapper"])
-      .then(function (data) {
-        callback(null, data, key, options);
+      .then(function (type) {
+        callback(null, {type: type, name: options.name});
       });
   }
 
-  var writeJson = function (err, scraper, key, options) {
-    var filename = "./data/destination_pages.json";
+  var writeJson = function (err, scraper, key, options, callback) {
+    // var destinationsFile = "./data/destination_pages.json";
 
-    if (err) {
-      throw err;
-    }
-    options.scraper = scraper;
-    airlines[key] = options;
+    // if (err) {
+    //   throw err;
+    // }
+    // options.scraper = scraper;
+    // airlines[key] = options;
 
-    if (_.every(airlines, "scraper")) {
-      fs.writeFile(filename,
-        JSON.stringify(airlines, null, 2),
-        function (err) {
-          if (err) {
-            throw err;
-          }
-          console.log("Saved %s", filename);
-        }
-      );
-    }
+    // if (_.every(airlines, "scraper")) {
+    //   fs.writeFile(destinationsFile,
+    //     JSON.stringify(airlines, null, 2),
+    //     function (err) {
+    //       if (err) {
+    //         throw err;
+    //       }
+    //       console.log("Saved %s", destinationsFile);
+    //     }
+    //   );
+    // }
   };
 
-  // getRoutes(airlines[0], writeJson);
+  // getScraperType(airlines[0], writeJson);
 
-  async.forEachOf(airlines, function (value, key, callback) {
-    getRoutes(value, key, writeJson);
-    callback();
-  }, function (err) {
+  async.map(airlines, function (options, callback) {
+    getScraperType(options, callback);
+  },  function (err, results) {
     if (err) {
       throw err;
     }
+    airlines = _.reduce(results, function(airlines, result){
+      var index = _.findIndex(airlines,{name: result.name});
+
+      airlines[index].scraper = result.type;
+      return airlines;
+    }, airlines);
+    fs.writeFileSync(destinationsFile, JSON.stringify(airlines,null,2));
+    console.log("Saved %s", destinationsFile);
   });
