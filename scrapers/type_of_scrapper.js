e250077f3ea3c04e53a30e89458f0f4bc7e39604
cristobal-io <me@cristobal.io>
2015-11-19 23:40:02 +0100

    evolution

--- scrapers/type_of_scrapper.js
+++ scrapers/type_of_scrapper.js
@@ -3,7 +3,6 @@
 module.exports = function ($) {
   var scraper = "default";
   
-  debugger;
   if ($("[id^='Scheduled_destinations_from']").length) {
     scraper = "table_with_origins";
   } else if ($(".sortable").hasClass("sortable")) {
