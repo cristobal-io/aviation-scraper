676720b82792b6c2941edfc7c8442e496b2e0b16
cristobal-io <me@cristobal.io>
2015-12-04 20:58:35 +0100

    server test not working when mocha -w

--- test/models.test.js
+++ test/models.test.js
@@ -6,6 +6,7 @@ var expect = chai.expect;
 var express = require("express");
 var serveStatic = require("serve-static");
 var app = express();
+var http = require("http");
 // scraper
 var sjs = require("scraperjs");
 // constants
@@ -16,20 +17,47 @@ var SERVER_LISTENING = BASE_URL + ":" + PORT;
 
 describe("server", function () {
 
-  app.use(serveStatic(__dirname + MODELS_DIR));
-  console.log("server started.");
-  app.listen(PORT);
+  debugger;
+  before("tests", function (done) {
+    // body...
+    startServer();
+    done();
+  });
 
-  it("Should match h1 from index.html", function (done) {
+  it("Should match H1 from index.html", function (done) {
 
     sjs.StaticScraper.create(SERVER_LISTENING)
       .scrape(function ($) {
         return $("h1").text();
       })
       .then(function (data) {
-        expect(data).to.eql("this is a test file");
+        expect(data).to.eql("Models");
         done();
       });
   });
 
+  it("True or false test", function () {
+    expect(true).to.be.true;
+  });
+
 });
+
+function getStatusCode() {
+  var statusCode = http.get(SERVER_LISTENING, function (res) {
+    return res.statusCode ;
+  });
+
+  return statusCode;
+}
+
+function startServer() {
+
+  if (getStatusCode() !== 200) {
+    app.use(serveStatic(__dirname + MODELS_DIR));
+    console.log("server started.");
+    app.listen(PORT);
+    console.log("listening");
+  } else {
+    console.log("status code: 200");
+  }
+}
