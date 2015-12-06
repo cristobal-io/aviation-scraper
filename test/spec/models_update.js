9ac25e89257848375be5632a8c441c7f5ff71b9e
cristobal-io <me@cristobal.io>
2015-12-06 14:31:31 +0100

    added method to download functional webpage examples

--- test/spec/models_update.js
+++ test/spec/models_update.js
@@ -1,3 +1,6 @@
+/*eslint no-unused-vars: 0*/
+/*global stderr */
+
 "use strict";
 
 var sjs = require("scraperjs");
@@ -30,3 +33,48 @@ function getRoutes() {
 }
 
 getRoutes();
+
+
+// courtesy of:
+// http://www.hacksparrow.com/using-node-js-to-download-files.html
+
+// Dependencies
+// var fs = require("fs");
+var url = require("url");
+// var http = require("http");
+var exec = require("child_process").exec;
+// var spawn = require("child_process").spawn;
+
+// App variables
+var file_url = "http://upload.wikimedia.org/wikipedia/commons/4/4f/Big%26Small_edit_1.jpg";
+var DOWNLOAD_DIR = "./downloads/";
+
+// We will be downloading the files to a directory, so make sure it"s there
+// This step is not required if you have manually created the directory
+
+var mkdir = "mkdir -p " + DOWNLOAD_DIR;
+var child = exec(mkdir, function (err, stdout, stderr) {
+  if (err) {
+    throw err;
+  } else {
+    download_file_wget(file_url);
+  }
+});
+
+// Function to download file using wget
+var download_file_wget = function (file_url) {
+
+  // extract the file name
+  var file_name = url.parse(file_url).pathname.split("/").pop();
+  // compose the wget command
+  var wget = "wget -P " + DOWNLOAD_DIR + " " + file_url;
+  // excute wget using child_process" exec function
+
+  var child = exec(wget, function (err, stdout, stderr) {
+    if (err) {
+      throw err;
+    } else {
+      console.log(file_name + " downloaded to " + DOWNLOAD_DIR);
+    }
+  });
+};
