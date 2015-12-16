3ebf60cb8b1d6f24a66ffca7bef78188c683d4b8
cristobal-io <me@cristobal.io>
2015-12-16 18:05:26 +0100

    changed from wget to https.get

--- test/spec/models_update.js
+++ test/spec/models_update.js
@@ -1,11 +1,7 @@
 "use strict";
 
-// courtesy of:
-// http://www.hacksparrow.com/using-node-js-to-download-files.html
-
 // Dependencies
 var url = require("url");
-var exec = require("child_process").exec;
 var async = require("async");
 
 // App variables
@@ -16,91 +12,39 @@ var file_url = [
   "Aegean_Airlines_destinations"
 ];
 var DOWNLOAD_DIR = "./test/spec/models/";
-var DESTINATIONS_URL = "https://en.wikipedia.org/wiki/Category:Lists_of_airline_destinations";
-
-// // Function to download file using wget
-// var download_file_wget = function (file_url, callback) {
-//   if (file_url.indexOf("http") === -1) {
-//     file_url = BASE_URL + file_url;
-//   }
-
-//   // extract the file name
-//   var file_name = url.parse(file_url).pathname.split("/").pop();
-
-//   // compose the wget command
-//   var wget = "wget -P " + DOWNLOAD_DIR + " " + file_url + " --no-check-certificate";
-
-//   // var wget = "wget -p -k " + DOWNLOAD_DIR + " " + file_url;
-//   // excute wget using child_process" exec function
-
-
-//   var child = exec(wget, function (err, stdout, stderr) { //eslint-disable-line no-unused-vars 
-//     if (err) {
-//       throw err;
-//     } else {
-//       console.log(file_name + " downloaded to " + DOWNLOAD_DIR); // eslint-disable-line no-console
-//       callback();
-//     }
-//   });
-
-// };
-
-// async.map(file_url, download_file_wget, function () {
-//   console.log("Async finished"); // eslint-disable-line no-console
-// });
-
-// download_file_wget(DESTINATIONS_URL, function () {
-//   console.log("destinations file downloaded"); //eslint-disable-line no-console
-// });
+// var DESTINATIONS_URL = "https://en.wikipedia.org/wiki/Category:Lists_of_airline_destinations";
 
 // Dependencies
 var fs = require("fs");
-var http = require("http");
+var https = require("https");
 
-// Function to download file using HTTP.get
-var download_file_httpget = function (file_url) {
-  if (file_url.indexOf("http") === -1) {
+// Function to download file using HTTPs.get
+var download_file_httpsGet = function (file_url, callback) {
+  if (file_url.indexOf("https") === -1) {
     file_url = BASE_URL + file_url;
   }
   console.log(file_url);
-  var options = {
-    host: url.parse(file_url).host,
-    port: 80,
-    path: url.parse(file_url).pathname
-  };
 
-  console.log(options);
-  var file_name = url.parse(file_url).pathname.split("/").pop();
+  var file_name = url.parse(file_url).pathname.split("/").pop() + ".html";
 
   console.log("filename: " + file_name);
   var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
 
-  var req = http.request(options, function (res) {
-    console.log("STATUS: " + res.statusCode);
-    console.log("HEADERS: " + JSON.stringify(res.headers));
-    res.on("data", function (data) {
-      console.log(data);
-      file.write(data);
+  https.get(file_url, function (res) {
+    res.on("data", function (chunk) {
+      file.write(chunk);
     }).on("error", function (err) {
       throw err;
     }).on("end", function () {
       file.end();
       console.log(file_name + " downloaded to " + DOWNLOAD_DIR);
+      callback();
     });
   });
 
-  req.on("error", function (err) {
-    throw err;
-  });
-  req.end();
 };
 
-download_file_httpget(file_url[0], function () {
-  console.log("destinations file downloaded"); //eslint-disable-line no-console
-});
-
-var request = require("request");
-
-request("http://www.sitepoint.com", function(error, response, body) {
-  console.log("sitepoint: "+ body);
-});
+async.map(file_url, download_file_httpsGet, function (err) {
+  if (err) {throw err}
+  console.log("\nModels updated."); //eslint-disable-line no-console
+} );
