237ba012bf71e7df24716a98f057340da28108d5
cristobal-io <me@cristobal.io>
2015-12-19 14:15:49 +0100

    ajv spikes json validator

--- spikes/schema/ajv/index.js
+++ spikes/schema/ajv/index.js
@@ -1,12 +1,13 @@
 "use strict";
-var Ajv = require("ajv");
-var ajv = Ajv(); 
 
 var data = require("../example.json");
 var schema = require("../schema_example.json");
 
 console.log("data: ", JSON.stringify(data, null, 2));
 console.log("schema: ", JSON.stringify(schema, null, 2));
+
+var Ajv = require("ajv");
+var ajv = Ajv(); 
 
 var validate = ajv.compile(schema);
 var valid = validate(data);
