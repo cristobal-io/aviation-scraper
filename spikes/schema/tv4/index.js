5b1ece385d6e271e2acdb93592d71fff0ab41ff6
cristobal-io <me@cristobal.io>
2015-12-19 14:11:16 +0100

    schema tv4 proper error handling

--- spikes/schema/tv4/index.js
+++ spikes/schema/tv4/index.js
@@ -7,4 +7,6 @@ var schema = require("../schema_example.json");
 console.log("data: ", JSON.stringify(data, null, 2));
 console.log("schema: ", JSON.stringify(schema, null, 2));
 console.log(tv4.validate(data, schema));
-console.log("data 2 error: " + JSON.stringify(tv4.error, null, 2));
+if (tv4.error) {
+  console.log("data 2 error: " + JSON.stringify(tv4.error, null, 2));
+}
