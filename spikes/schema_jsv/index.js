69efd2b2746e95948c409ba382219b9cc94e1e5e
cristobal-io <me@cristobal.io>
2015-12-19 10:42:12 +0100

    spike jsv

--- spikes/schema_jsv/index.js
+++ spikes/schema_jsv/index.js
@@ -2,40 +2,46 @@
 
 var JSV = require("JSV").JSV;
 
-var json = "hello";
-var schema = {
-  "type": "object"
-};
-var exampleJSON = require("./schema_example.json");
+var exampleJson = require("./example.json");
+var exampleSchema = require("./schema_example.json");
 var env = JSV.createEnvironment();
-var report = env.validate(json, schema);
 
-if (report.errors.length === 0) {
-  console.log("validation passed");
-} else {
-  console.log(report.errors[0].message);
-}
-// env = JSV.createEnvironment();
+console.log("exampleJson : ", JSON.stringify(exampleJson, null, 2));
 
-var example2 = env.validate({
-  a: 1
-}, {
-  type: "object",
-  properties: {
-    a: {
-      type: "string"
-    }
-  }
-});
-
-console.log("example2 errors: %s", JSON.stringify(example2.errors, null, 2));
-// env = JSV.createEnvironment();
-
-console.log(exampleJSON);
-var exampleReport = env.validate(json, exampleJSON);
+console.log("exampleSchema : ", JSON.stringify(exampleSchema, null, 2));
+var exampleReport = env.validate(exampleJson, exampleSchema);
 
 if (exampleReport.errors.length === 0) {
   console.log("validation passed");
 } else {
-  console.log("exampleJSON : ", JSON.stringify(exampleReport.errors, null, 2));
+  console.log("exampleSchema  errors: ", JSON.stringify(exampleReport.errors, null, 2));
 }
+
+
+// var schema = {
+//   "type": "object"
+// };
+
+// var report = env.validate(json, schema);
+
+// if (report.errors.length === 0) {
+//   console.log("validation passed");
+// } else {
+//   console.log(report.errors[0].message);
+// }
+// // env = JSV.createEnvironment();
+
+// var example2 = env.validate({
+//   a: 1
+// }, {
+//   type: "object",
+//   properties: {
+//     a: {
+//       type: "string"
+//     }
+//   }
+// });
+
+// console.log("example2 errors: %s", JSON.stringify(example2.errors, null, 2));
+// env = JSV.createEnvironment();
+
