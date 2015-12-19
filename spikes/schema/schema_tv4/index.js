16828b698db70a40f97c72edf3280a1fde51a0ac
cristobal-io <me@cristobal.io>
2015-12-19 13:46:58 +0100

    spikes

--- spikes/schema/schema_tv4/index.js
+++ spikes/schema/schema_tv4/index.js
@@ -1,8 +1,12 @@
 "use strict";
 var tv4 = require("tv4");
 
-var data = require("../schema_jsv/example.json");
-var schema = require("../schema_jsv/schema_example.json");
+var data = require("../example.json");
+var schema = require("../schema_example.json");
+
+console.log(data);
+console.log(schema);
+
 
 var valid = tv4.validate(data, schema);
 
