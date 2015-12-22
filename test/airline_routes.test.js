471096f55896787c65cf400be117f7f5c99f92d9
cristobal-io <me@cristobal.io>
2015-12-22 19:08:21 +0100

    created schema to validate routes json return

--- test/airline_routes.test.js
+++ test/airline_routes.test.js
@@ -2,16 +2,29 @@
 // Mocha
 var chai = require("chai");
 var expect = chai.expect;
+// dependencies
+var _ = require("lodash");
 
 var airlinesIndex = require("../src/index.js");
 var getRoutes = airlinesIndex.getRoutes;
 var getAllRoutes = airlinesIndex.getAllRoutes;
 
+var Ajv = require("ajv");
+var ajv = Ajv();
+
 var options = require("./fixtures/scraper_options.json");
 
 
-describe("Airline_routes.js: \n", function() {
-  
+describe("Airline_routes.js: \n", function () {
+  var validateScraperTableSchema;
+
+  before(function (done) {
+    var scraperTableOriginSchema = require("../schema/destinations_table_origin.schema.json");
+
+    validateScraperTableSchema = ajv.compile(scraperTableOriginSchema);
+    done();
+  });
+
   describe("getRoutes function", function () {
 
     it("Should be a function", function () {
@@ -25,10 +38,15 @@ describe("Airline_routes.js: \n", function() {
       });
     });
 
-    it("Should return an array from table_with_origins scraper model", function (done) {
+    it.only("Should return an array from table_with_origins scraper model", function (done) {
       // TODO: make it return an object and specify the schema all scrapers must follow.
       getRoutes(options[1], function (err, results) {
-        expect(results).to.be.an("object");
+
+        // console.log(results);
+        var valid = validateScraperTableSchema(results);
+
+        expect(valid, _.get(validateScraperTableSchema, "errors[0].message") ).to.be.true;
+        // expect(results).to.be.an("object");
         done();
       });
     });
@@ -51,4 +69,3 @@ describe("Airline_routes.js: \n", function() {
 
   });
 });
-
