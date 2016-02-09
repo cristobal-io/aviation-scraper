86fd0bcf3a2ff6d76364137bfa289bd9a122a90e
cristobal-io <me@cristobal.io>
2016-02-09 14:14:41 +0100

    refactoring

--- scrapers/index.js
+++ scrapers/index.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   "default": require("./scraper.default.js"),
-  "default_variant": require("./default_variant.js"),
+  "default_variant": require("./scraper.default_variant.js"),
   "table": require("./scraper.table.js"),
-  "table_with_origins": require("./table_with_origins.js"),
+  "table_with_origins": require("./scraper.table_with_origins.js"),
   "table_toccolours": require("./scraper.table_toccolours.js"),
   "table_center": require("./scraper.table_center.js"),
   "table_tablesorter": require("./scraper.table_tablesorter.js"),
   "type_of_scrapper": require("./type_of_scrapper.js"),
   "destinations": require("./scraper.destinations.js"),
-  "destinations_link": require("./destinations_link.js"),
-  "airports": require("./airports.scraper.js")
+  "destinations_link": require("./scraper.destinations_link.js"),
+  "airports": require("./scraper.airports.js")
 };
