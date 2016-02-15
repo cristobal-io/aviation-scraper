4de022381a37304670092d25558675634950a424
cristobal-io <me@cristobal.io>
2016-02-15 16:23:33 +0100

    spike to run split get airports with child exec

--- spikes/child_process/split_get_airports_data.js
+++ spikes/child_process/split_get_airports_data.js
@@ -1,9 +1,10 @@
 "use strict";
 var source = require("../../src/");
 var splitGetAirportsData = source.splitGetAirportsData;
+var airportslistComplete = require("../../data/airports_list.json");
 var airportLinks = require("../../test/fixtures/airport_links.json");
 
-splitGetAirportsData(airportLinks, function(err, stdout) {
+splitGetAirportsData(airportslistComplete, function(err, stdout) {
   if (err) {console.log(err);}
   var result = stdout.toString();
   console.log(result.split("\n"));
