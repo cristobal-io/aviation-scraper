60b85c3d517d3ae26564e4307a2dd007aee507b2
cristobal-io <me@cristobal.io>
2016-02-16 15:19:07 +0100

    added test for getAirportsByIata with a single link to iata list pages

--- test/airports_iata.test.js
+++ test/airports_iata.test.js
@@ -12,21 +12,45 @@ var ajv = Ajv();
 var _ = require("lodash");
 
 
-describe("airports_iata.js", function() {
-  
-  it("should get all the airports", function() {
-    var iataLocalList = require("./fixtures/airports_iata.data.json"), i;
+describe("airports_iata.js \n", function () {
 
-    for (i = 0; i < iataLocalList.length; i+=1) {
-      iataLocalList[i]= "http://localhost:3000/" + iataLocalList[i];
-    }
-    getAllAirportsByIata(iataLocalList, function(err, airportsData) {
-      var airportsSchema = require("../schema/airport_links.schema.json");
-      var validateAirportsLinkSchema = ajv.compile(airportsSchema);
-      var validAirportsLink = validateAirportsLinkSchema(airportsData);
+  describe("getAllAirportsByIata", function () {
 
-      expect(validAirportsLink, _.get(validateAirportsLinkSchema, "errors[0].message")).to.be.true;
+    it("should get all the airports", function () {
+      var iataLocalList = require("./fixtures/airports_iata.data.json"),
+        i;
+
+      for (i = 0; i < iataLocalList.length; i += 1) {
+        iataLocalList[i] = "http://localhost:3000/" + iataLocalList[i];
+      }
+      getAllAirportsByIata(iataLocalList, function (err, airportsData) {
+        var airportsSchema = require("../schema/airport_links.schema.json");
+        var validateAirportsLinkSchema = ajv.compile(airportsSchema);
+        var validAirportsLink = validateAirportsLinkSchema(airportsData);
+
+        expect(validAirportsLink, _.get(validateAirportsLinkSchema, "errors[0].message")).to.be.true;
+      });
     });
   });
+
+  describe("getAirportsByIata", function () {
+
+    it("should return a single file with the proper schema when passing only one link to the iata list.", function () {
+      var iataLocalList = require("./fixtures/airports_iata.data.json");
+      var airportsSchema = require("../schema/airport_links.schema.json");
+      var validateAirportsLinkSchema = ajv.compile(airportsSchema);
+
+
+      getAirportsByIata("http://localhost:3000/" + iataLocalList[0], function (err, airports) {
+        var validAirportsLink = validateAirportsLinkSchema(airports);
+
+        expect(validAirportsLink, _.get(validateAirportsLinkSchema, "errors[0].message")).to.be.true;
+      });
+
+    });
+
+
+  });
+
 
 });
