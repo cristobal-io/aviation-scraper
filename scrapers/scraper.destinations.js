3736e862538d4b7f5337075c9efd9a262fc95e59
cristobal-io <me@cristobal.io>
2016-02-18 16:41:37 +0100

    refactoring destinations scraper

--- scrapers/scraper.destinations.js
+++ scrapers/scraper.destinations.js
@@ -4,12 +4,10 @@ module.exports = function ($) {
   var destinations = [];
 
   $(".mw-category li a").map(function () {
-    var airline = {
+    destinations.push({
       name: $(this).text().replace(/ destinations$/, ""),
       destinationsLink: $(this).attr("href")
-    };
-
-    destinations.push(airline);
+    });
   });
   return destinations;
 };
